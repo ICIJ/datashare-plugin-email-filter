f7820dee94dde1de579e0e7cdb795a79670dbfb7
Pierre Romera <hello@pirhoo.com>
2024-06-13 16:44:40 +0000

    fix: wait for datashare to be ready

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-datashare.registerFilter({
-  type: 'FilterNamedEntity',
-  options: {
-    name: 'namedEntityEmail',
-    key: 'byMentions',
-    category: 'EMAIL',
-    isSearchable: true
-  }
+document.addEventListener('datashare:ready', ({ detail: { core } }) => {
+  core.registerFilter({
+    type: 'FilterNamedEntity',
+    options: {
+      order: 65,
+      name: 'namedEntityEmail',
+      key: 'byMentions',
+      category: 'EMAIL',
+      isSearchable: true
+    }
+  })
 })
-
